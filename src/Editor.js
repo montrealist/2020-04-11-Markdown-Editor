57c0bb2d425f2ed8e598f5fe6860fcd7301cba54
Max <montrealist@users.noreply.github.com>
2020-04-11 13:15:52 -0400

    switched to theme that syntax hightlights markdown

--- src/Editor.js
+++ src/Editor.js
@@ -1,15 +1,14 @@
 import React from "react";
 import AceEditor from "react-ace";
-import "ace-builds/src-noconflict/mode-css";
 import "ace-builds/src-noconflict/mode-markdown";
-import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-chrome";
 
 const Editor = (props) => {
     return (
         <>
             <AceEditor
                 mode="markdown"
-                theme="monokai"
+                theme="chrome"
                 onChange={props.onChange}
                 value={props.value}
                 setOptions={{
