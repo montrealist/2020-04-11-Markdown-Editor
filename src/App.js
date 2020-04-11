e524f7683423456b5b6cf1a84598537569cf2be0
Max <montrealist@users.noreply.github.com>
2020-04-11 14:59:34 -0400

    prettying up with Bulma hero element

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Editor from "./Editor";
 import { sample } from "./SampleMarkdown";
 
 import "react-bulma-components/dist/react-bulma-components.min.css";
-import { Columns, Content, Dropdown } from "react-bulma-components";
+import {
+    Columns,
+    Container,
+    Content,
+    Dropdown,
+    Hero,
+    Navbar,
+} from "react-bulma-components";
 
 const ReactMarkdown = require("react-markdown");
 
@@ -29,28 +36,57 @@ function App() {
 
     return (
         <div className="app">
-            <header className="app-header">
-                Markdown Editor
-                <Dropdown
-                    value={theme}
-                    onChange={setTheme}
-                    label="Select editor theme:"
-                >
-                    {themes.map((theme) => (
-                        <Dropdown.Item value={theme}>{theme}</Dropdown.Item>
-                    ))}
-                </Dropdown>
-            </header>
-            <Columns>
-                <Columns.Column className="editor" size="half">
-                    <Editor theme={theme} value={input} onChange={setInput} />
-                </Columns.Column>
-                <Columns.Column size="half">
-                    <Content>
-                        <ReactMarkdown source={input} />
-                    </Content>
-                </Columns.Column>
-            </Columns>
+            <Navbar fixed="top">
+                <Navbar.Brand>
+                    <Navbar.Item renderAs="a" href="/">
+                        Markdown Editor
+                    </Navbar.Item>
+                    <Navbar.Burger />
+                </Navbar.Brand>
+                <Navbar.Menu>
+                    <Navbar.Container>
+                        <Navbar.Item href="#">
+                            <Dropdown
+                                value={theme}
+                                onChange={setTheme}
+                                label="Select editor theme:"
+                            >
+                                {themes.map((theme) => (
+                                    <Dropdown.Item value={theme}>
+                                        {theme}
+                                    </Dropdown.Item>
+                                ))}
+                            </Dropdown>
+                        </Navbar.Item>
+                    </Navbar.Container>
+                    <Navbar.Container position="end">
+                        <Navbar.Item href="https://github.com/montrealist/2020-04-11-Markdown-Editor">
+                            Repo on Github
+                        </Navbar.Item>
+                    </Navbar.Container>
+                </Navbar.Menu>
+            </Navbar>
+            <Hero color="primary" className="is-fullheight-with-navbar">
+                <Hero.Body>
+                    <Container>
+                        <Columns>
+                            <Columns.Column className="editor" size="half">
+                                <Editor
+                                    theme={theme}
+                                    value={input}
+                                    onChange={setInput}
+                                />
+                            </Columns.Column>
+                            <Columns.Column size="half">
+                                <Content>
+                                    <ReactMarkdown source={input} />
+                                </Content>
+                            </Columns.Column>
+                        </Columns>
+                    </Container>
+                </Hero.Body>
+                {/* <Hero.Foot>Foot</Hero.Foot> */}
+            </Hero>
         </div>
     );
 }
